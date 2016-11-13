7caeb7a0c05a3ed9bf7815d985d550a3c1ddc694
Hiroki Ata <hiroki.ata@gmail.com>
2016-11-13 20:44:48 +0900

    Fixed a bug for compare_set separator
    This may need to refactor to handle configuration symbol or string well.

--- lib/key_value_checker/checker_config.rb
+++ lib/key_value_checker/checker_config.rb
@@ -36,7 +36,9 @@ module KeyValueChecker
     def load_map(map_config)
       map_config.each do |key, value|
         intern_key = key.intern
-        if @rule_config[intern_key]
+        if intern_key == :config
+          merge_default_config(intern_key, value)
+        elsif @rule_config[intern_key]
           merge_top_config(intern_key, value)
         else
           @rule_config[intern_key] = value
@@ -46,6 +48,12 @@ module KeyValueChecker
 
     private
 
+    def merge_default_config(config_key, new_value)
+      return unless new_value
+      new_value = replace_key_to_symbol(new_value)
+      @rule_config[config_key] = merge_map_config(@rule_config[config_key], new_value)
+    end
+
     def merge_top_config(intern_key, new_value)
       @rule_config[intern_key] = merge_map_config(@rule_config[intern_key], new_value)
     end
@@ -54,6 +62,18 @@ module KeyValueChecker
       return new_map unless old_map
       return old_map unless new_map
       old_map.merge(new_map)
+    end
+
+    def replace_key_to_symbol(kv_map)
+      return kv_map unless kv_map.is_a?(Hash)
+
+      new_map = {}
+      # Note: each cannot be used because this code recall the same func
+      # and it causes an error.
+      for elem in kv_map.to_a do
+        new_map[elem[0].intern] = replace_key_to_symbol(elem[1])
+      end
+      new_map
     end
   end
 end
